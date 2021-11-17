47c7b4ceea5eab0c4e3fec04b1ac2849cbe5e556
sandeepdarsan <sandeep.darsan@gmail.com>
2021-11-17 17:39:28 +0530

    initial components

---
+++ src/main/java/com/sd/seleneai/browsers/Browser.java
@@ -0,0 +1,5 @@
+package com.sd.seleneai.browsers;
+
+public class Browser {
+
+}
---
+++ src/test/java/com/sd/seleneai/sampleTest.java
@@ -0,0 +1,14 @@
+package com.sd.seleneai;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class sampleTest {
+
+    @Test
+    public void test(){
+        System.out.println("Sample Unit Test");
+        WebDriver driver = new FirefoxDriver();
+    }
+}
